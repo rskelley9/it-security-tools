6a249011e0c42e0d81a9089b7be1a4bf0a040ce9
Ryan Kelley <rskelley9@gmail.com>
2015-06-10 15:42:02 -0400

    re-arranged directories, added hashing directory in encryption_and_hashing directory (changed from encryption); added simple_pass_cracker.rb file with ruby script for simple password cracking using gentle-brute

---
+++ encryption_and_hashing/hashing/simple_pass_cracker.rb
@@ -0,0 +1,14 @@
+# WIP
+
+# require 'digest/md5'
+# require 'gentle_brute'
+
+# target_hash = '58e53d1324eef6265fdb97b08ed9aadf'
+
+# b = GentleBrute::BruteForcer.new
+# while true
+#   phrase = b.next_valid_phrase
+#   attempt_hash = Digest::MD5.hexdigest(phrase)
+#   puts "Password is #{phrase}" if attempt_hash == target_hash
+#   break if attempt_hash == target_hash
+# end
