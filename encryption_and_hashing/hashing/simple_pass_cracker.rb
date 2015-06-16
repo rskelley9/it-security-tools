6d61b626f2098d84729d2f7f2be8f2f3292d6783
Ryan Kelley <rskelley9@gmail.com>
2015-06-16 08:48:30 -0400

    added example note to pass cracker ruby file

--- encryption_and_hashing/hashing/simple_pass_cracker.rb
+++ encryption_and_hashing/hashing/simple_pass_cracker.rb
@@ -1,14 +1,16 @@
-# MD5 Hash Collision Cracker
+## MD5 Hash Brute Force Cracker
 
 require 'digest/md5'
 require 'gentle_brute'
 
+## Example
+# target_hash = Digest::MD5.hexdigest("poop")
 
 # Set default password hash if user doesn't enter one
 if ARGV[0]
 	target_hash = ARGV[0]
 else
-	target_hash = '58e53d1324eef6265fdb97b08ed9aadf'
+	target_hash = '098f6bcd4621d373cade4e832627b4f6'
 end
 
 word_list = GentleBrute::BruteForcer.new
@@ -18,16 +20,16 @@ puts message
 num_tries = 0
 
 while true
-	# Pending message every 10 tries
+	## Pending message every 10 tries
 	num_tries += 1
 	puts message if (num_tries % 10).eql? 0
 
-	# Use a word from word list to test
+	## Use a word from word list to test
 	phrase = word_list.next_valid_phrase
-  # Hash that word to compare to password hash
+  ## Hash that word to compare to password hash
   attempt_hash = Digest::MD5.hexdigest(phrase)
 
-  # If the hashes match, print the unhashed phrase (password)
+  ## If the hashes match, print the unhashed phrase (password)
   if attempt_hash == target_hash
   	puts "Cracked!"
   	puts "Password is #{phrase}"
