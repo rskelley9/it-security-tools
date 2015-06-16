fe933164937e5ec4cc0630d3432487f70ae4538b
Ryan Kelley <rskelley9@gmail.com>
2015-06-16 00:14:34 -0400

    updated password cracker to be more interactive; added gemfile to make installing dependencies one-step process; added passwords.txt test file; added notes.md with notes on how to use

---
+++ encryption_and_hashing/hashing/notes.md
@@ -0,0 +1,10 @@
+Test Run
+--------
+
+```
+$ bundle install
+
+$ wget https://raw.github.com/jamespenguin/gentle-brute/master/passwords.txt
+
+$ GentleBrute --crack-md5-list passwords.txt
+```
--- encryption_and_hashing/hashing/simple_pass_cracker.rb
+++ encryption_and_hashing/hashing/simple_pass_cracker.rb
@@ -1,14 +1,31 @@
-## WIP: This file is a work-in-progress
+# MD5 Hash Collision Cracker
 
-# require 'digest/md5'
-# require 'gentle_brute'
+require 'digest/md5'
+require 'gentle_brute'
 
-# target_hash = '58e53d1324eef6265fdb97b08ed9aadf'
+target_hash = '58e53d1324eef6265fdb97b08ed9aadf'
 
-# b = GentleBrute::BruteForcer.new
-# while true
-#   phrase = b.next_valid_phrase
-#   attempt_hash = Digest::MD5.hexdigest(phrase)
-#   puts "Password is #{phrase}" if attempt_hash == target_hash
-#   break if attempt_hash == target_hash
-# end
+word_list = GentleBrute::BruteForcer.new
+
+message = "Cracking password..."
+num_tries = 0
+while true
+	# Pending message every 100 tries
+	num_tries += 1
+	puts message if (num_tries % 100).eql? 1
+
+	# Use a word from word list to test
+	phrase = word_list.next_valid_phrase
+  # Hash that word to compare to password hash
+  attempt_hash = Digest::MD5.hexdigest(phrase)
+
+  # If the hashes match, print the unhashed phrase (password)
+  if attempt_hash == target_hash
+  	puts "Cracked!"
+  	puts "Password is #{phrase}"
+  end
+
+  break if attempt_hash == target_hash
+	puts "Tried #{attempt_hash}, not a match. Continuing trying to crack..."
+
+end
