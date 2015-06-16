b63175fda6b6beed8f710bc130251bbde1dad7f5
Ryan Kelley <rskelley9@gmail.com>
2015-06-16 00:22:16 -0400

    updated console message to print tried phrase instead of hashed phrase

--- encryption_and_hashing/hashing/simple_pass_cracker.rb
+++ encryption_and_hashing/hashing/simple_pass_cracker.rb
@@ -8,11 +8,13 @@ target_hash = '58e53d1324eef6265fdb97b08ed9aadf'
 word_list = GentleBrute::BruteForcer.new
 
 message = "Cracking password..."
+puts message
 num_tries = 0
+
 while true
-	# Pending message every 100 tries
+	# Pending message every 10 tries
 	num_tries += 1
-	puts message if (num_tries % 100).eql? 1
+	puts message if (num_tries % 10).eql? 0
 
 	# Use a word from word list to test
 	phrase = word_list.next_valid_phrase
@@ -26,6 +28,6 @@ while true
   end
 
   break if attempt_hash == target_hash
-	puts "Tried #{attempt_hash}, not a match. Continuing trying to crack..."
+	puts "Tried #{phrase}, not a match. Still trying to crack..."
 
 end
